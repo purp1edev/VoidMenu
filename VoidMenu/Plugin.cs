1d6a9b44d4f0990923076ff093181e97c5800600
purp1edev <69186938+purp1edev@users.noreply.github.com>
2021-07-05 14:35:26 +1000

    Add project files.

---
+++ VoidMenu/Plugin.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using IPA;
+using IPA.Config;
+using IPA.Config.Stores;
+using BeatSaberMarkupLanguage.Settings;
+using UnityEngine;
+using IPALogger = IPA.Logging.Logger;
+
+namespace VoidMenu
+{
+
+    [Plugin(RuntimeOptions.SingleStartInit)]
+    public class Plugin
+    {
+        internal static Plugin Instance { get; private set; }
+        internal static IPALogger Log { get; private set; }
+
+        [Init]
+        /// <summary>
+        /// Called when the plugin is first loaded by IPA (either when the game starts or when the plugin is enabled if it starts disabled).
+        /// [Init] methods that use a Constructor or called before regular methods like InitWithConfig.
+        /// Only use [Init] with one Constructor.
+        /// </summary>
+        public void Init(IPALogger logger, Config conf)
+        {
+            Instance = this;
+            Log = logger;
+
+            Configuration.PluginConfig.Instance = conf.Generated<Configuration.PluginConfig>();
+            BSMLSettings.instance.AddSettingsMenu("VoidMenu", "VoidMenu.Configuration.settings.bsml", Configuration.PluginConfig.Instance);
+
+            Log.Info("VoidMenu initialized.");
+        }
+
+        [OnStart]
+        public void OnApplicationStart()
+        {
+            Log.Debug("OnApplicationStart");
+            new GameObject("VoidMenuController").AddComponent<VoidMenuController>();
+
+        }
+
+        [OnExit]
+        public void OnApplicationQuit()
+        {
+            Log.Debug("OnApplicationQuit");
+
+        }
+    }
+}
---
+++ VoidMenu/VoidMenuController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using BS_Utils.Utilities;
+
+namespace VoidMenu
+{
+    /// <summary>
+    /// Monobehaviours (scripts) are added to GameObjects.
+    /// For a full list of Messages a Monobehaviour can receive from the game, see https://docs.unity3d.com/ScriptReference/MonoBehaviour.html.
+    /// </summary>
+    public class VoidMenuController : MonoBehaviour
+    {
+        public static VoidMenuController Instance { get; private set; }
+
+        #region Monobehaviour Messages
+        // Only ever called once, mainly used to initialize variables.
+        private void Awake()
+        {
+            // For this particular MonoBehaviour, we only want one instance to exist at any time, so store a reference to it in a static property
+            //   and destroy any that are created while one already exists.
+            if (Instance != null)
+            {
+                Plugin.Log?.Warn($"Instance of {GetType().Name} already exists, destroying.");
+                GameObject.DestroyImmediate(this);
+                return;
+            }
+            GameObject.DontDestroyOnLoad(this); // Don't destroy this object on scene changes
+            Instance = this;
+            Plugin.Log?.Debug($"{name}: Awake()");
+
+            BSEvents.lateMenuSceneLoadedFresh += LateMenuSceneLoaded;
+        }
+        // Called when the script is being destroyed.
+        private void OnDestroy()
+        {
+            Plugin.Log?.Debug($"{name}: OnDestroy()");
+            if (Instance == this)
+                Instance = null; // This MonoBehaviour is being destroyed, so set the static instance property to null.
+
+        }
+        #endregion
+
+        void LateMenuSceneLoaded(ScenesTransitionSetupDataSO setupDataSO)
+        {
+            if (Configuration.PluginConfig.Instance.enabled)
+            {
+                GameObject.Find("MenuEnvironmentManager").SetActive(false);
+            }
+        }
+    }
+}
